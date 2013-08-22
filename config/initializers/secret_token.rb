db8417438ffa81350e3195c1098ad8df43e58625
明尾 知洋 <h2.meo.0829@gmail.com>
2013-08-22 19:37:38 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeoApp::Application.config.secret_key_base = '9dd407014c062efeb265cd6d5591b2a019639e2d1bf393647e3fb785387ce8b98be5bb53eaa1a00b900f8714708244cf0cdad4219a30001f0f15d787fd60630f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeoApp::Application.config.session_store :cookie_store, key: '_meo_app_session'
